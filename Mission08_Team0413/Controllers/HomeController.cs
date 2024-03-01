2f323dfcb7f7fcf3485a38969c4e6d6e56c28f17
HammondCameron55 <110644424+HammondCameron55@users.noreply.github.com>
2024-02-29 17:17:51 -0700

    Revert "Added field for user to add a task description"
    This reverts commit 01970e095b5dcde7e1c92c50395a6fbec3025529.

--- Mission08_Team0413/Controllers/HomeController.cs
+++ Mission08_Team0413/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using Mission08_Team0413.Models;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
 
-
+//Test to see if Github is working
 
 namespace Mission08_Team0413.Controllers
 {
